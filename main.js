b33f4951148e0903d9255fe96dd166aece09be2b
doctorbratan <63260425+doctorbratan@users.noreply.github.com>
2021-12-14 18:13:27 +0200

    Pre-Start

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
+const swipe = document.getElementById('swipe');
+var scrollBollean = false;
+swipe.style.display =  "none";
+
+window.addEventListener('scroll', function() {
+    
+    if (window.scrollY >= 120) {
+        scrollBollean = true
+        swipe.style.display =  "none"
+    }
+
+})
+
+window.onload = function() {
+    setTimeout(() => {
+        if (!scrollBollean) {
+            swipe.style.display =  "block"
+        }
+    }, 10000);
+}
+
+function ScrollToMenu() {
+    EPPZScrollTo.scrollVerticalToElementById('logo', 0)
+
+}
+
 const component = new Vue({
     el: '#main',
     data: { 
